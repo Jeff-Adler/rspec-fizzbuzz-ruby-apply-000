c3d90b444b4eae647e055c55fec8a935d3559e44
Jeff Adler <jeffm.adler@gmail.com>
2020-05-27 19:47:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0 & if int % 5 == 0
+    "fizzbuzz" # Go fizzbuz
+  elsif int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
+  elsif int % 5 == 0 # if the number int is divisible by 5
+    "buzz" # Go buzz
+  else
+    return nil
   end
-  ifelse int % 5 == 0 # if the number int is divisible by 3
-    "Buzz" # Go fizz
-  end
-else
-end
 end
