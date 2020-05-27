40f7fb02667d22eb0d9b78191edcb21378bb1070
Jeff Adler <jeffm.adler@gmail.com>
2020-05-27 19:55:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if (int % 3 == 0 && int % 5 == 0)
-    "fizzBuzz" # Go fizzbuz
+    "FizzBuzz" # Go fizzBuzz
   elsif int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+    "Fizz" # Go Fizz
   elsif int % 5 == 0 # if the number int is divisible by 5
-    "Buzz" # Go buzz
+    "Buzz" # Go Buzz
   else
     return nil
   end
